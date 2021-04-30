f9ab87a48e1675868342d9cb64dbd15c9e4e0f30
Roman Prilepsky <r616on@gmail.com>
2021-04-30 22:12:50 +0300

    task_v1.1_Done

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,6 +1,15 @@
 export default class Character {
   constructor(name, type) {
-    this.name = name;
+    if (typeof name === "string") {
+      if (name.length > 2 && name.length < 10) {
+        this.name = name;
+      } else {
+        throw new Error("Длинна имени не верна");
+      }
+    } else {
+      throw new Error("Неправильный тип данных");
+    }
+
     this.type = type;
     this.health = 100;
     this.level = 1;
--- src/js/__tests__/person.test.js
+++ src/js/__tests__/person.test.js
@@ -9,25 +9,31 @@ import {
 
 test("test Bowerman", () => {
   const data = { attack: 25, defence: 25, type: "Bowerman" };
-  expect(new Bowerman("B")).toEqual(expect.objectContaining(data));
+  expect(new Bowerman("pers")).toEqual(expect.objectContaining(data));
 });
 test("test Swordsman", () => {
   const data = { attack: 40, defence: 10, type: "Swordsman" };
-  expect(new Swordsman("B")).toEqual(expect.objectContaining(data));
+  expect(new Swordsman("pers")).toEqual(expect.objectContaining(data));
 });
 test("test Magician", () => {
   const data = { attack: 10, defence: 40, type: "Magician" };
-  expect(new Magician("B")).toEqual(expect.objectContaining(data));
+  expect(new Magician("pers")).toEqual(expect.objectContaining(data));
 });
 test("test Daemon", () => {
   const data = { attack: 10, defence: 40, type: "Daemon" };
-  expect(new Daemon("B")).toEqual(expect.objectContaining(data));
+  expect(new Daemon("pers")).toEqual(expect.objectContaining(data));
 });
 test("test Undead", () => {
   const data = { attack: 25, defence: 25, type: "Undead" };
-  expect(new Undead("B")).toEqual(expect.objectContaining(data));
+  expect(new Undead("pers")).toEqual(expect.objectContaining(data));
 });
 test("test Zombie", () => {
   const data = { attack: 10, defence: 40, type: "Zombie" };
-  expect(new Zombie("B")).toEqual(expect.objectContaining(data));
+  expect(new Zombie("pers")).toEqual(expect.objectContaining(data));
+});
+test("test error the length of the name is incorrect", () => {
+  expect(() => new Zombie("p")).toThrow("Длинна имени не верна");
+});
+test("test error the type of the name is incorrect", () => {
+  expect(() => new Zombie({})).toThrow("Неправильный тип данных");
 });
